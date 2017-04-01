7661bb77e0312d9624a3e083f0815437711a8e45
Rickard Sunden <rickard.sunden@outlook.com>
2017-04-01 19:59:59 -0400

    add ability for title key pairs

--- app/views/skriva/posts/hello.md
+++
@@ -1,9 +0,0 @@
-title: hello
-description: hey how are you?
-keywords: hi, hello, hi
-
-# Hello
-
-```ruby
-sup = "hello"
-```
--- lib/skriva/post_helpers/instance_methods.rb
+++ lib/skriva/post_helpers/instance_methods.rb
@@ -6,18 +6,61 @@ module Skriva
 
       def initialize(slug:)
         @slug = slug
-        markdown = Redcarpet::Markdown.new(Skriva::HTML, fenced_code_blocks: true)
-        lines = File.readlines(Rails.root.join('app', 'views', 'skriva', 'posts', "#{parse_slug}.md"))
-        @title = lines.first.try(:chomp)
-        @description = lines.second.try(:chomp)
-        @keywords = lines.third.try(:chomp)
-        @content = lines[3..-1].try(:join)
-        if @content
-          @html = markdown.render(@content).html_safe
+        @markdown = Redcarpet::Markdown.new(Skriva::HTML, fenced_code_blocks: true)
+        setup
+      end
+
+      def headers
+        @headers ||= begin
+          headers = lines[0..content_start].select do |line|
+            line.chomp.present?
+          end
+          headers.each do |header|
+            colon = header.index(":")
+            key = header[0..colon - 1].strip
+            value = header[colon + 1..-1].strip
+            define_singleton_method "#{key}" do
+              value
+            end
+          end
+        end
+      end
+
+      def content
+        lines[content_start..-1]
+      end
+
+      def html
+        if @html.nil? && content.present?
+          @html = @markdown.render(content.join).html_safe
+        else
+          @html
         end
       end
 
       private
+
+        def lines
+          @lines ||= File.readlines(Rails.root.join('app', 'views', 'skriva', 'posts', "#{parse_slug}.md"))
+        end
+
+        def setup
+          headers
+          content
+        end
+
+        def content_start
+          @content_start ||= begin
+            start = nil
+            @lines.each_with_index do |line, index|
+              if line.include?("{{ content }}")
+                @lines.delete_at(index)
+                start = index
+              end
+            end
+            start
+          end
+        end
 
         def parse_slug
           @slug
