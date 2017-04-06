79739551c084a2c54911402abce5e1c899d12d8a
Rickard Sunden <rickard.sunden@outlook.com>
2017-04-05 21:44:53 -0400

    fix bad params logic

--- app/controllers/skriva/posts_controller.rb
+++ app/controllers/skriva/posts_controller.rb
@@ -2,16 +2,22 @@ module Skriva
   class PostsController < ApplicationController
 
     def index
-      if params[:tags].blank?
+      if post_params.empty?
         @posts = Skriva::Post.all
       else
-        @posts = Skriva::Post.where(params.slice(*Post.headers))
+        @posts = Skriva::Post.where(post_params)
       end
     end
 
     def show
       @post = Skriva::Post.new(file_name: params[:slug])
     end
+
+    private
+
+      def post_params
+        params.slice(*Post.headers)
+      end
 
   end
 end
